ffc3c58f3e59bfe1179f2e3a9750e022bf2189cb
Tim Veil <tjveil@gmail.com>
2018-11-20 18:53:39 -0500

    updating yarn.scheduler.capacity.maximum-am-resource-percent

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting resourcemanager!"
-${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} -Dyarn.scheduler.capacity.maximum-am-resource-percent=.5 resourcemanager
+${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} -Dyarn.scheduler.capacity.maximum-am-resource-percent=.25 resourcemanager
